e3414b1edc7d0f868323a74f0e0498aa97768e6a
Nathan Ho <nathanho21@gmail.com>
2023-02-22 13:38:07 +1100

    use bs4

--- scraper.py
+++ scraper.py
@@ -2,61 +2,50 @@
 # including some code snippets below that you should find helpful
 
 import scraperwiki
-import lxml.html
 import datetime
+from bs4 import BeautifulSoup
 
 base_html = "http://www.planningportal.nsw.gov.au"
 
 #get html from source
 def get_applications(page):
-	# print("Visiting page {}...".format(page))
 	#pretend to be an ipad to bypass cloudflare protection with user-agent
 	userAgent = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
 	return scraperwiki.scrape("http://www.planningportal.nsw.gov.au/major-projects/projects?status=Exhibition&page={}".format(page),"",userAgent) #on exhibition only!
 
 #extract needed data from html
 def get_data(data):
-	root = lxml.html.fromstring(data).find('body')
-	applications = root.cssselect("div[class='card__content']")
-	appDict = []
-	for i in applications:
-		refId = i.cssselect("div[class='field field-field-case-id field-type-string field-label-hidden']")
-		address = i.cssselect("div")
-		council = i.cssselect("span[class='card__sub']")
-		name = i.cssselect("h3[class='card__title']")
-		link = i.cssselect("a[href]")
+	page = BeautifulSoup(data, "html.parser")
+	applications = []
+	for table in page.find_all(class_="card__content"):
+		refId = table.find(class_ = "field field-field-case-id field-type-string field-label-hidden").get_text().strip()
+		address = table.find(class_ = "card__title").find_next_sibling().get_text().strip()
+		council = table.find(class_ = "card__sub").get_text().strip()
+		name = table.find(class_ = "card__title").get_text().strip()
+		link = table.find("a", href = True)["href"]
 		time = datetime.datetime.now().strftime("%x")
+		# print("Reference ID:\t", refId)
+		# print(">\tAddress:\t", address)
+		# print(">\tCouncil:\t", council)
+		# print(">\tDescription:\t", name)
+		# print(">\tLink:\t", link)
+		# print(">\tTime:\t", time)
+		# print("-------------------------------")
 		thisApplication = {
-			"council_reference" : ''.join(refId[0].itertext()).strip(),
-			"address" : ''.join(address[6].itertext()).strip(),
-			"council" : ''.join(council[0].itertext()).strip(),
-			"description" : ''.join(name[0].itertext()).strip(),
-			"info_url" : base_html + ''.join(link[0].items()[0][1]),
+			"council_reference" : refId,
+			"address" : address,
+			"council" : council,
+			"description" : name,
+			"info_url" : base_html + link,
 			"date_scraped" : time
 		}
-		# print("\tApplication was scraped successfully!")
-		# save space by saving each application here
-
-		# try:
-		# 	store_data(thisApplication)
-		# except Exception as e:
-		# 	print("Could not save\n")
-		# 	raise e
-        # # print(thisApplication, "\n\n")
-		appDict.append(thisApplication)
-	return appDict
-		
+		applications.append(thisApplication)
+	return applications
 
 def store_data(application):
-	print(application)
 	scraperwiki.sql.save(unique_keys=['council_reference'], data=application, table_name="data")
 
-# def store_db(data):
-# 	for application in data:
-# 		print(application)
-# 		scraperwiki.sql.save(unique_keys=['council_reference'], data=application, table_name="data")
-	
-def main():
+def visit_pages():
 	applications = []
 	page = 0
 	html = get_applications(page)
@@ -66,8 +55,13 @@ def main():
 		page = page + 1
 		html = get_applications(page)
 		apps = get_data(html)
-	# for app in applications:
-	# 	print(app, "\n")
+	return applications
+
+def main():
+	applications = visit_pages()
+	
+	for app in applications:
+		print(app)
 	quit()
 
 main()
